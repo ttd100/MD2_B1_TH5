e623655ae92d6a0dfc1956173e8f7df70302691e
Your Name <dat054760@gmail.com>
2022-07-24 23:17:30 +0700

    up

---
+++ src/main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("which month that you want to count days?");
+        int month =scanner.nextInt();
+        switch (month){
+            case 2:
+                System.out.println("The month "+month+"has 28 or 29 days!");
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("the month"+month+"has 31 days!");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("the month "+month+"has 30 days!");
+                break;
+            default:
+                System.out.println("this month doesn't exist!" );
+        }
+    }
+}
